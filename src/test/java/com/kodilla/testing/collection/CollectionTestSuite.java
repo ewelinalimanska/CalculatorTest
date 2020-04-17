42c9933f3e6596786e3160eff7336f0ca7a9cf47
ewelinalimanska <limanskaewelina@gmail.com>
2020-04-17 17:08:16 +0000

    test commit

---
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.collection;
+import org.junit.*;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+
+public class CollectionTestSuite {
+
+    public void testOddNumbersExterminatorEmptyList(){
+
+
+    }
+    public void testOddNumbersExterminatorNormalList(){
+
+    }
+}
+
